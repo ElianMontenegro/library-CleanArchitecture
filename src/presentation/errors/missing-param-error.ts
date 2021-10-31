3b2c669d90220266a19da90ee50e5f2303a1aee2
unknown <elianmontenegro491@gmail.com>
2021-10-30 21:43:30 -0300

    feat: add missingParamError in errors

---
+++ src/presentation/errors/missing-param-error.ts
@@ -0,0 +1,7 @@
+
+export class MissingParamError extends Error {
+    constructor (private paramName : string) {
+      super(`Missing param: ${paramName}`)
+      this.name = `MissingParamError: ${paramName}`
+    }
+}
