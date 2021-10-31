2e15cfcff1dfa0d8bc28630f58878ae28a1ad59f
unknown <elianmontenegro491@gmail.com>
2021-10-31 15:10:07 -0300

    feat: add noContent and serverError to http-status-helper

--- src/presentation/helpers/http-status-helper.ts
+++ src/presentation/helpers/http-status-helper.ts
@@ -1,6 +1,18 @@
 import { HttpResponse } from "../protocols";
 
+
+
+export const noContent = (): HttpResponse => ({
+    statusCode: 204,
+    body: null
+})
+
 export const badRequest = (error: Error): HttpResponse => ({
     statusCode: 400,
+    body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+    statusCode: 500,
     body: error
 })
