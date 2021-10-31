c626459957bf840962db259df15829ecfbe4c0f8
unknown <elianmontenegro491@gmail.com>
2021-10-31 00:39:16 -0300

    feat: add Book DTO to Dtos

---
+++ src/domain/DTOs/book-input-DTO.ts
@@ -0,0 +1,12 @@
+
+export interface book {
+    title : string
+    autor : string
+    category : string
+    lenguage : string[]
+    country : string
+    isbn : string
+    year : number
+    numberPage : number
+    editorial : string
+}
