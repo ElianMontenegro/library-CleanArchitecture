059628231f116c78e0e1068ed265875f540797e8
unknown <elianmontenegro491@gmail.com>
2021-11-18 20:31:36 -0300

    refactor: add addAccountRepository in addAccount usecases

--- src/data/usecases/account/addAccountUseCase.ts
+++ src/data/usecases/account/addAccountUseCase.ts
@@ -1,4 +1,4 @@
-import { CheckAccountByEmailRepository } from "@/data/protocols/db/account";
+import { CheckAccountByEmailRepository, AddAccountRepository } from "@/data/protocols/db/account";
 import { Hasher } from "@/data/protocols/criptography";
 import { accountInputDTO } from "@/domain/DTOs";
 import { AddAccount } from "@/domain/usecases";
@@ -6,14 +6,17 @@ import { AddAccount } from "@/domain/usecases";
 export class AddAccountUseCase implements AddAccount{
     constructor (
         private readonly checkAccountByEmailRepository : CheckAccountByEmailRepository,
-        private readonly hasher : Hasher
+        private readonly hasher : Hasher,
+        private readonly addAccountRepository : AddAccountRepository
     ) {}
     async add(user: accountInputDTO): Promise<Boolean>{
         const account = await this.checkAccountByEmailRepository.checkAccountByEmail(user.email)
+        let isSaved : Boolean = false
         if(account){
-            const passwordHash = this.hasher.hash(user.password);
-            return true
+            const passwordHash = await this.hasher.hash(user.password);
+            isSaved = await this.addAccountRepository.add({... user, password :  passwordHash})
+            return isSaved
         }
-        return null
+        return isSaved
     }
 }
