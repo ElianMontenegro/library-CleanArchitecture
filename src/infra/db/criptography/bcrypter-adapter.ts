7ee0057ca5ffe974fb62641843bc762076256237
unknown <elianmontenegro491@gmail.com>
2021-11-22 19:01:02 -0300

    refactor: add compare to BcrypterAdapter

--- src/infra/db/criptography/bcrypter-adapter.ts
+++ src/infra/db/criptography/bcrypter-adapter.ts
@@ -1,9 +1,13 @@
-import { Hasher } from "@/data/protocols/criptography";
+import { HashCompare, Hasher } from "@/data/protocols/criptography";
 import bcrypt from 'bcrypt'
-export class BcrypterAdapter implements Hasher{
+export class BcrypterAdapter implements Hasher, HashCompare{
     constructor(private readonly salt : number){}
 
     async hash(plaintext: string): Promise<string>{
         return await bcrypt.hash(plaintext, this.salt)
+    }
+
+    async compare(plainText: string, hashText: string): Promise<Boolean>{
+        return await bcrypt.compare(plainText, hashText)
     }
 }
