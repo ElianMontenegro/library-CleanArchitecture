f58db76580651ee5f1c8dce7d29491239908a859
unknown <elianmontenegro491@gmail.com>
2021-11-21 23:19:09 -0300

    refactor: check if password match in AuthenticateUseCase

--- src/data/usecases/account/autheticate-use-case.ts
+++ src/data/usecases/account/autheticate-use-case.ts
@@ -1,13 +1,23 @@
 import { Authenticate } from "@/domain/usecases/account";
 import { LoadAccountByEmailRepository  } from '@/data/protocols/db/account'
+import { HashCompare } from '@/data/protocols/criptography'
 
 export class AuthenticateUseCase implements Authenticate{
     constructor(
-        private readonly loadAccountByEmailRepository : LoadAccountByEmailRepository
+        private readonly loadAccountByEmailRepository : LoadAccountByEmailRepository,
+        private readonly hashCompare : HashCompare
     ){}
     async auth(params: Authenticate.Params):  Promise<Authenticate.Result>{
         const account = await this.loadAccountByEmailRepository.load(params.email);
-        if(account){}
+        if(account){
+            const isValid = await this.hashCompare.compare(params.password, account.password)
+            if(isValid){
+                return {
+                    accessToken : "",
+                    refreshToken: ""
+                }
+            }
+        }
         return null
     }
 
