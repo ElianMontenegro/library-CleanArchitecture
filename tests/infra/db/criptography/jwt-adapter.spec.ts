7a1a7c3e991f11b75ae556c3739fb55ae81c1df7
unknown <elianmontenegro491@gmail.com>
2021-11-22 23:05:27 -0300

    test: ensure JWTAdapter sign call with correct params

--- tests/infra/db/criptography/jwt-adapter.spec.ts
+++ tests/infra/db/criptography/jwt-adapter.spec.ts
@@ -1,5 +1,5 @@
 import { JWTGenerator, JWTAdapter } from '../../../../src/infra/db/criptography'
-
+import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken', () => ({
     sign(params : JWTGenerator.Params){
         return 'token'
@@ -12,10 +12,20 @@ const makeSut = () => {
 
 describe('JWTAdapter', () => {
     describe('sign accessToken', () => {
-        test('Should return a access token if on sign success', async () => {
+        test('Should return a access token if on sign success', () => {
             const sut = makeSut()
             const token = sut.sign({id : 'any_id', secret : 'any_secret', expiresIn : 'any_expiresIn'})
             expect(token).toBe('token')
         })
+
+        test('Should call sign with correct params', () => {
+            const sut = makeSut()
+            const signSpy = jest.spyOn(jwt, 'sign')
+            sut.sign({id : 'any_id', secret : 'any_secret', expiresIn : 'any_expiresIn'})
+            expect(signSpy).toBeCalledWith({id : 'any_id'}, 'any_secret', 'any_expiresIn')
+        })
+
     })
 })
+
+
