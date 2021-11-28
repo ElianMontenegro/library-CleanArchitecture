3712fe3d9aa622077f1700316f1760a423d8e9db
unknown <elianmontenegro491@gmail.com>
2021-11-27 22:28:16 -0300

    feat: add load repository in account-mongo-repository

--- src/infra/db/mongo/account-mongo-repository.ts
+++ src/infra/db/mongo/account-mongo-repository.ts
@@ -1,11 +1,12 @@
-import { CheckAccountByEmailRepository, AddAccountRepository } from '@/data/protocols/db/account'
-import { mongoHelper } from '../../../../src/infra/db/mongo'
+import { CheckAccountByEmailRepository, AddAccountRepository, LoadAccountByEmailRepository } from '@/data/protocols/db/account'
+import { mongoHelper } from '@/infra/db/mongo'
 import { Collection } from 'mongodb'
 
 export class AccountMongoRepository implements 
     CheckAccountByEmailRepository,
-    AddAccountRepository {
-
+    AddAccountRepository,
+    LoadAccountByEmailRepository {
+  
     accountCollection : Collection
     makeCollection = () =>{
         this.accountCollection = mongoHelper.getCollection('user')
@@ -26,6 +27,25 @@ export class AccountMongoRepository implements
     async add(user: AddAccountRepository.Params): Promise<Boolean>{
         const isSave = await this.makeCollection().insertOne({...user, role : "user"})
         return isSave.insertedId !== null
+    }
+
+    async load(email: string): Promise<LoadAccountByEmailRepository.Result>{
+        const account = await this.makeCollection().findOne({
+            email
+        },{
+            projection: {
+                _id : 1,
+                username : 1,
+                password : 1
+            }
+        })
+        if(account){
+            return {
+                id : account._id,
+                username : account.username,
+                password : account.password
+            }
+        }
     }
 
 }
